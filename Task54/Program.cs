98d4f583ceed58429443f6c1457ad56c89a709f0
Igor Shpenev <vdvshneek@yandex.ru>
2022-11-22 17:18:58 +0500

    Completed task number 54

---
+++ Task54/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] MatrixArray(int a, int b)
+{
+    int[,] array = new int[a, b];
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            array[i, j] = new Random().Next(1, 9);
+        }
+    }
+    return array;
+}
+
+void PrintArr(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] Qwe(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+
+            for (int x = 0; x < array.GetLength(1) - 1; x++)
+            {
+                if (array[i, x] < array[i, x + 1])
+                {
+                    int temp = 0;
+                    temp = array[i, x];
+                    array[i, x] = array[i, x + 1];
+                    array[i, x + 1] = temp;
+                }
+            }
+
+        }
+
+    }
+
+    return array;
+}
+
+
+Console.Write("Input number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] Arr = MatrixArray(rows, columns);
+PrintArr(Arr);
+Qwe(Arr);
+PrintArr(Arr);
+
+
+
+
+
